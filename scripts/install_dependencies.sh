6e1b598cde5f5b10c752950efd26ada171934ad1
Martin Rosales Figueroa <106180137+codelock54@users.noreply.github.com>
2023-06-30 01:08:38 -0500

    add template

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,5 @@
-sudo apt install javascript html5 css bootstrap jquery json
+#sudo apt install javascript html5 css bootstrap jquery json
 sudo apt install -y httpd
+
+sudo apt-get install npm nginx nodejs -y 
+npm install bootstrap
